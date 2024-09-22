a4d571ce32eba63a5362e89b4a480688a275b471
Thieu Viet Hoang <thieuhoangent@gmail.com>
2024-09-22 22:49:25 +0700

    up shop component

--- src/app/features/shop/sidebar-shop/sidebar-shop.component.ts
+++ src/app/features/shop/sidebar-shop/sidebar-shop.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-sidebar-shop',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // Thêm FormsModule vào imports
   templateUrl: './sidebar-shop.component.html',
   styleUrl: './sidebar-shop.component.css'
 })
 export class SidebarShopComponent {
-
+  giaTri: number = 3000000; // Khởi tạo giá trị ban đầu cho slider
 }
