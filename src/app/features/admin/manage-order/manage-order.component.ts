466c3ebce56a32364b76b3248758bc7cbd0e699b
TeHD <dangkhoa014@gmail.com>
2024-10-21 00:12:36 +0700

    Update goods-receipt and product page

--- src/app/features/admin/manage-order/manage-order.component.ts
+++ src/app/features/admin/manage-order/manage-order.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminFooterComponent } from '../../../shared/components/admin-footer/admin-footer.component';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { OrderComponent } from '../dialog/order/order.component';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
@@ -11,18 +8,19 @@ import { ToastModule } from 'primeng/toast';
 import { DropdownModule } from 'primeng/dropdown';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
 @Component({
   selector: 'app-manage-order',
   standalone: true,
-  imports: [AdminFooterComponent, CommonModule, TableModule, InputTextModule, ButtonModule, ToastModule, DropdownModule, ConfirmDialogModule],
+  imports: [AdminFooterComponent, CommonModule, TableModule, InputTextModule, ButtonModule, ToastModule, DropdownModule, ConfirmDialogModule, DialogModule],
   providers: [MessageService, ConfirmationService],
   templateUrl: './manage-order.component.html',
   styleUrl: './manage-order.component.css'
 })
-export class ManageOrderComponent implements OnInit{
-  dataSource:any;
-  responseMessage:any;
-  orders = [
+export class ManageOrderComponent{
+  visible: boolean = false;
+
+  orders: Order[] = [
     {
       orderId: '000001',
       customerId: '00001',
@@ -31,7 +29,21 @@ export class ManageOrderComponent implements OnInit{
       orderDate: new Date('2024-05-19T05:04:42'),
       discount: -20000,
       totalAmount: 7605000,
-      status: 0 // 0 là chưa xác nhận, 1 là đã xác nhận
+      status: 0, // 0 là chưa xác nhận, 1 là đã xác nhận
+      orderDetails: [
+        {
+          productId: 'P001',
+          productName: 'Sản phẩm 1',
+          quantity: 2,
+          price: 3000000
+        },
+        {
+          productId: 'P002',
+          productName: 'Sản phẩm 2',
+          quantity: 1,
+          price: 1605000
+        }
+      ]
     },
     {
       orderId: '000002',
@@ -41,14 +53,27 @@ export class ManageOrderComponent implements OnInit{
       orderDate: new Date('2024-05-20T06:00:00'),
       discount: -15000,
       totalAmount: 5000000,
-      status: 1
+      status: 1,
+      orderDetails: [
+        {
+          productId: 'P003',
+          productName: 'Sản phẩm 3',
+          quantity: 1,
+          price: 5000000
+        }
+      ]
     },
     // Thêm các dữ liệu mẫu khác
   ];
 
-  constructor(
-    private dialog: MatDialog,
-    private router:Router) { }
+  selected_order: Order | null = null;
+
+  constructor() { }
+
+    showDialog(order: Order) {
+      this.visible = true;
+      this.selected_order = order;
+    }
   
   handleInput(event: Event, dt: any): void {
     const inputElement = event.target as HTMLInputElement;
@@ -56,27 +81,23 @@ export class ManageOrderComponent implements OnInit{
         dt.filterGlobal(inputElement.value, 'contains');
     }
   }
-
-  handleViewDetailAction() {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.maxWidth = '100%'; // Set the max width of the dialog
-    dialogConfig.data = {
-      action: 'Add'
-    };
-    const dialogRef = this.dialog.open(OrderComponent, dialogConfig);
-    this.router.events.subscribe(() => {
-      dialogRef.close();
-    });
-  }
-
-  ngOnInit(): void {
-    (window as any).actionEvents = {
-      'click .viewOrder': function (e: any, value: any, row: any, index: any) {
-        // Tham chiếu tới hàm trong Angular
-        const orderComponent = new ManageOrderComponent(this.dialog, this.router);
-        orderComponent.handleViewDetailAction();
-      }
-    };
-  }
 }
 
+interface Order {
+  orderId: string;
+  customerId: string;
+  discountCode: string;
+  address: string;
+  orderDate: Date;
+  discount: number;
+  totalAmount: number;
+  status: number;
+  orderDetails: OrderDetail[];
+}
+
+interface OrderDetail {
+  productId: string;
+  productName: string;
+  quantity: number;
+  price: number;
+}
