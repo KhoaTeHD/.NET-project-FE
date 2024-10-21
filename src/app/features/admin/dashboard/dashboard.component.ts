7f08b479b459343e4b64bb9b39e6972f7372dd70
KhoaTeHD <dangkhoa014@gmail.com>
2024-10-21 15:43:46 +0700

    Khoa hotfix

--- src/app/features/admin/dashboard/dashboard.component.ts
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -1,17 +1,186 @@
 import { Component } from '@angular/core';
 import { RevenueComponent } from "./revenue/revenue.component";
 import { ProductStaticComponent } from "./product-static/product-static.component";
-import Chart from 'chart.js/auto';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { CommonModule } from '@angular/common';
+import { SliderModule } from 'primeng/slider';
+import { ButtonModule } from 'primeng/button';
+import { Table } from 'primeng/table';
+import { FormsModule } from '@angular/forms';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { DropdownModule } from 'primeng/dropdown';
 
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RevenueComponent, ProductStaticComponent],
+  imports: [RevenueComponent, ProductStaticComponent, TableModule, TagModule, InputTextModule, ProgressBarModule, CommonModule, SliderModule, ButtonModule, FormsModule, MultiSelectModule, DropdownModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  
+
+  customers: Customer[] = [
+    {
+      id: 1000,
+      name: 'James Butt',
+      country: {
+        name: 'Algeria',
+        code: 'dz',
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 77,
+      representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png',
+      },
+      balance: 70663,
+    },
+    {
+      id: 1000,
+      name: 'James Butt',
+      country: {
+        name: 'Algeria',
+        code: 'dz',
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 17,
+      representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png',
+      },
+      balance: 70663,
+    },
+    {
+      id: 1000,
+      name: 'James Butt',
+      country: {
+        name: 'Algeria',
+        code: 'dz',
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 38,
+      representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png',
+      },
+      balance: 70663,
+    },
+    {
+      id: 1000,
+      name: 'James Butt',
+      country: {
+        name: 'Algeria',
+        code: 'dz',
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 17,
+      representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png',
+      },
+      balance: 70663,
+    },
+  ];
+
+  representatives: Representative[] = [
+    { name: 'Amy Elsner', image: 'amyelsner.png' },
+    { name: 'Anna Fali', image: 'annafali.png' },
+    { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+    { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+    { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+    { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+    { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+    { name: 'Onyama Limba', image: 'onyamalimba.png' },
+    { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+    { name: 'Xuxue Feng', image: 'xuxuefeng.png' }
+  ];
+
+  statuses: any[] = [
+    { label: 'Unqualified', value: 'unqualified' },
+    { label: 'Qualified', value: 'qualified' },
+    { label: 'New', value: 'new' },
+    { label: 'Negotiation', value: 'negotiation' },
+    { label: 'Renewal', value: 'renewal' },
+    { label: 'Proposal', value: 'proposal' }
+  ];
+
+  loading: boolean = false;
+
+  activityValues: number[] = [0, 100];
+
+  searchValue: string | undefined;
+
+
+  clear(table: Table) {
+    table.clear();
+    this.searchValue = ''
+  }
+
+  handleInput(event: Event, dt: any): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement) {
+      dt.filterGlobal(inputElement.value, 'contains');
+    }
+  }
+
+  getSeverity(status: string) {
+    switch (status.toLowerCase()) {
+      case 'unqualified':
+        return 'danger';
+
+      case 'qualified':
+        return 'success';
+
+      case 'new':
+        return 'info';
+
+      case 'negotiation':
+        return 'warning';
+
+      case 'renewal':
+        return undefined;
+      default:
+        return undefined;
+    }
+  }
+}
+
+interface Country {
+  name: string;
+  code: string;
+}
+
+interface Representative {
+  name: string;
+  image: string;
+}
+
+interface Customer {
+  id: number;
+  name: string;
+  country: Country;
+  company: string;
+  date: string;
+  status: string;
+  verified: boolean;
+  activity: number;
+  representative: Representative;
+  balance: number;
 }
