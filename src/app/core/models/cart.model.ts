a849d745fa45ef821c7b080e1e9b9294e3f17463
Thieu Viet Hoang <thieuhoangent@gmail.com>
2024-11-28 23:26:32 +0700

    fix cart model

--- src/app/core/models/cart.model.ts
+++ src/app/core/models/cart.model.ts
@@ -1,8 +1,11 @@
+import { ProductVariationDto } from './productVariation.model';
+
 export interface CartDto {
   item_Id?: number;
   cus_Id?: string;
   price?: number;
   quantity?: number;
+  productVariation?: ProductVariationDto;
 }
 
 export interface CartDtoExtendStatus extends CartDto {
